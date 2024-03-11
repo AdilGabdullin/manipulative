e91e469954ecbe9db0fcb120e21a5c505b29bc2c
Adil Gabdullin <izenbard@gmail.com>
2024-03-11 19:53:24 +0500

    bottom toolbar redesign

--- src/components/BottomToolbar.jsx
+++ src/components/BottomToolbar.jsx
@@ -2,49 +2,39 @@ import { useAppStore } from "../state/store";
 
 export const bottomToolbarHeight = 60;
 
-const bgColor = "#072846";
-const buttonColor = "#20496d";
+const bgColor = "#c1cedb";
 
 const BottomToolbar = () => {
   const state = useAppStore();
-  const style = { height: bottomToolbarHeight - 10, backgroundColor: bgColor, padding: 5 };
   return (
-    <div style={style}>
-      <Button text="undo" onClick={state.undo} />
-      <Button text="redo" onClick={state.redo} />
-      <Button text="clear" onClick={state.clear} />
-      <Button text="zoom in" onClick={() => state.setScale(state.scale - 0.1)} />
-      <Button text="zoom out" onClick={() => state.setScale(state.scale + 0.1)} />
-      <Button text="fullscreen" onClick={state.toggleFullscreen} />
+    <div className="bottom-toolbar" style={{ height: bottomToolbarHeight }}>
+      <div className="bottom-toolbar-buttons">
+        <Button text="undo" imageSrc="buttons/undo-min.png" onClick={state.undo} />
+        <Button text="redo" imageSrc="buttons/redo-min.png" onClick={state.redo} />
+        <Button text="clear" imageSrc="buttons/clear-canvas-min.png" onClick={state.clear} />
+        <Button text="zoom in" imageSrc="buttons/zoom-in-min.png" onClick={() => state.setScale(state.scale - 0.1)} />
+        <Button text="zoom out" imageSrc="buttons/zoom-out-min.png" onClick={() => state.setScale(state.scale + 0.1)} />
+        <Button text="fullscreen" imageSrc="buttons/full-screen-min.png" onClick={state.toggleFullscreen} />
 
-      <Button text="brush" onClick={state.toggleBrush} />
-      <Button text="eraser" onClick={state.toggleEraser} />
-
+        <Button text="brush" imageSrc="buttons/pencil-min.png" onClick={state.toggleBrush} />
+        <Button text="eraser" imageSrc="buttons/eraser-min.png" onClick={state.toggleEraser} />
+      </div>
       {state.mode == "geoboard" && (
-        <select onChange={(e) => state.setWorkspace(e.target.value)} value={state.workspace} style={{ fontSize: 20 }}>
-          <option value="square">Square</option>
-          <option value="isometric">Isometric</option>
-          <option value="circular">Circular</option>
-        </select>
+        <div className="workspace-selector">
+          <label className="workspace-selector-label">Workspace</label>
+          <select onChange={(e) => state.setWorkspace(e.target.value)} value={state.workspace}>
+            <option value="square">Square</option>
+            <option value="isometric">Isometric</option>
+            <option value="circular">Circular</option>
+          </select>
+        </div>
       )}
     </div>
   );
 };
 
-const Button = (props) => {
-  const style = {
-    fontSize: "16px",
-    backgroundColor: buttonColor,
-    color: "#FFFFFF",
-    height: bottomToolbarHeight - 2 * (5 + 8),
-    padding: 8,
-    margin: 8,
-  };
-  return (
-    <button style={style} onClick={props.onClick}>
-      {props.text}
-    </button>
-  );
+const Button = ({ onClick, imageSrc, text }) => {
+  return <img src={"./img/" + imageSrc} height={32} onClick={onClick} />;
 };
 
 export default BottomToolbar;
