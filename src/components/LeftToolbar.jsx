4312db002431837c0205d44d3f58d8d775032194
Adil Gabdullin <izenbard@gmail.com>
2024-03-29 12:43:53 +0500

    click placement

--- src/components/LeftToolbar.jsx
+++ src/components/LeftToolbar.jsx
@@ -6,9 +6,9 @@ import { Fragment } from "react";
 export const leftToolbarWidth = 180;
 
 const allOptions = [
-  { value: 1000000, color: "#c2185b" },
-  { value: 100000, color: "#afb42b" },
-  { value: 10000, color: "#3f51b5" },
+  { value: 1_000_000, color: "#c2185b" },
+  { value: 100_000, color: "#afb42b" },
+  { value: 10_000, color: "#3f51b5" },
   { value: 1000, color: "#9c27b0" },
   { value: 100, color: "#8bc34a" },
   { value: 10, color: "#e91e63" },
@@ -19,11 +19,10 @@ const allOptions = [
 ];
 
 const LeftToolbar = () => {
-  const state = useAppStore();
-  const { mode, origin, offset, scale } = state;
-  const [min, max] = mode == "Whole Numbers" ? [1, 1000000] : [0.001, 10];
+  const { mode, origin, offset, scale, addElement, height, lastActiveElement, elements } = useAppStore();
+  const [min, max] = mode == "Whole Numbers" ? [1, 1_000_000] : [0.001, 10];
   const options = allOptions.filter(({ value }) => value >= min && value <= max);
-  const top = (state.height - radius * 2 * options.length) / (options.length + 1);
+  const top = (height - radius * 2 * options.length) / (options.length + 1);
   const left = (leftToolbarWidth - 2 * radius) / 2;
 
   const onDragMove = (e) => {
@@ -40,19 +39,37 @@ const LeftToolbar = () => {
       value,
     };
     e.target.setAttrs({ ...pos, scaleX: 1.0, scaleY: 1.0 });
-    state.addElement(element);
+    addElement(element);
+  };
+
+  const onPointerClick = (value, color) => (e) => {
+    const last = elements[lastActiveElement];
+    const pos = last ? { x: last.x + radius * 2, y: last.y } : { x: 0, y: 0 };
+    addElement({
+      type: "disk",
+      color,
+      value,
+      ...pos,
+    });
   };
 
   return (
     <>
-      <Rect fill="#f3f9ff" x={0} y={0} width={leftToolbarWidth} height={state.height} />
+      <Rect fill="#f3f9ff" x={0} y={0} width={leftToolbarWidth} height={height} />
       {options.map(({ value, color }, i) => {
         const x = left + radius;
         const y = top + i * (top + radius * 2) + radius;
         return (
           <Fragment key={value}>
             <Disk value={value} x={x} y={y} color={color} />
-            <Group x={x} y={y} draggable onDragMove={onDragMove} onDragEnd={onDragEnd(value, color, { x, y })}>
+            <Group
+              x={x}
+              y={y}
+              draggable
+              onDragMove={onDragMove}
+              onDragEnd={onDragEnd(value, color, { x, y })}
+              onPointerClick={onPointerClick(value, color)}
+            >
               <Disk value={value} x={0} y={0} color={color} />
             </Group>
           </Fragment>
