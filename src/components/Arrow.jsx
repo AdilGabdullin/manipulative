bea3232a2c348cbdb6d770fdade7cdf13961db69
Adil Gabdullin <izenbard@gmail.com>
2024-04-02 18:07:24 +0500

    arrow horisontal resize

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,55 +1,61 @@
-import { Group, Path, Rect } from "react-konva";
+import { Arc, Group, Path, Rect } from "react-konva";
 import { colors } from "../state/colors";
 import { useAppStore } from "../state/store";
+import { useRef } from "react";
 
 const strokeWidth = 10;
 const headSize = 25;
-export const arWidth = 200;
-export const arHeight = 100;
+export const arWidth = 130;
+export const arHeight = 65;
 
 const Arrow = ({ id, x, y, width, height, isBlue, visible, locked }) => {
   const state = useAppStore();
-  const { origin, selectIds, relocateElement } = state;
+  const { origin, selectIds, relocateElement, updateElement } = state;
 
   const color = isBlue ? colors.blue : colors.red;
   const headX = isBlue ? width : 0;
-  const headY = height;
+  const headY = height + headSize / 4;
 
   const pos = {
     x: origin.x + x,
     y: origin.y + y,
   };
 
+  const arcData = (width, height) => `
+    m 0 0 
+    a ${width / 2} ${height} 0 0 1
+      ${width + strokeWidth}
+      ${0}
+    h ${-strokeWidth}
+    a ${width / 2 - strokeWidth} ${height - strokeWidth} 0 0 0
+      ${strokeWidth - width}
+      ${0}
+    h ${-strokeWidth}
+  `;
+
+  const group = useRef();
+  const arc = useRef();
   return (
     <Group
+      ref={group}
       id={id}
       x={pos.x}
       y={pos.y}
       visible={visible !== undefined ? visible : true}
       draggable
+      onDragMove={(e) => {
+        if (e.target === group.current) {
+        }
+      }}
       onDragEnd={(e) => {
-        relocateElement(id, e.target.x() - pos.x, e.target.y() - pos.y);
+        if (e.target === group.current) {
+          relocateElement(id, e.target.x() - pos.x, 0);
+        }
       }}
       onPointerClick={() => selectIds([id], locked)}
     >
       <Rect width={width} height={height} stroke={"black"} />
-      <Path
-        x={isBlue ? -strokeWidth / 2 : width + strokeWidth / 2}
-        y={height}
-        fill={color}
-        data={`
-          m 0 0 
-          a ${width / 2} ${height} 0 0 1
-            ${width - headSize / 4.5}
-            ${-headSize}
-          h ${-strokeWidth}
-          a ${width / 2 - strokeWidth} ${height - strokeWidth} 0 0 0
-            ${2 * strokeWidth - width + headSize / 4.5}
-            ${headSize}
-          h ${-strokeWidth}
-        `}
-        scaleX={isBlue ? 1.13 : -1.13}
-      />
+      <Path ref={arc} x={-strokeWidth / 2} y={height} fill={color} data={arcData(width, height)} />
       <Path
         x={headX}
         y={headY}
@@ -61,8 +67,32 @@ const Arrow = ({ id, x, y, width, height, isBlue, visible, locked }) => {
           l ${headSize} ${0}
           l ${-headSize / 2} ${headSize}
         `}
+        draggable
+        onDragMove={(e) => {
+          const x = e.target.x();
+          const dx = x - headX;
+          e.target.setAttrs({ y: headY });
+          const newWidth = isBlue ? headX + dx : width - x;
+          const newX = isBlue ? -strokeWidth / 2 : -strokeWidth / 2 + dx;
+          // const newIsBlue = dx > -width;
+          arc.current.setAttrs({
+            x: newX,
+            data: arcData(newWidth, height),
+            // fill: newIsBlue ? colors.blue : colors.red,
+          });
+        }}
+        onDragEnd={(e) => {
+          const x = e.target.x();
+          const dx = x - headX;
+          e.target.setAttrs({ x:0, y: headY });
+          const newWidth = isBlue ? headX + dx : width - x;
+          const newX = isBlue ? pos.x - origin.x : pos.x + dx - origin.x;
+          arc.current.setAttrs({
+            x: -strokeWidth / 2,
+          });
+          updateElement(id, { x: newX, width: newWidth });
+        }}
       />
-      <Rect width={width} height={height} stroke={"black"} />
     </Group>
   );
 };
