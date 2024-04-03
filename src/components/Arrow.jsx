dbcdb93355b1013a5bd9d7149f21b07c94170e0d
Adil Gabdullin <izenbard@gmail.com>
2024-04-03 14:21:06 +0500

    arrow refactor

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -8,6 +8,8 @@ const headSize = 25;
 export const arWidth = 130;
 export const arHeight = 65;
 const minHeight = 50;
+const rhWidth = 50;
+const rhHeight = strokeWidth;
 
 const Arrow = ({ id, x, y, width, height, isBlue, visible, locked }) => {
   const state = useAppStore();
@@ -42,6 +44,65 @@ const Arrow = ({ id, x, y, width, height, isBlue, visible, locked }) => {
   const arc = useRef();
   const rect = useRef();
   const head = useRef();
+
+  const groupDragMove = (e) => {
+    if (e.target === group.current) {
+      // TODO magnet
+    }
+  };
+
+  const groupDragEnd = (e) => {
+    if (e.target === group.current) {
+      relocateElement(id, e.target.x() - pos.x, e.target.y() - pos.y);
+    }
+  };
+
+  const groupClick = () => selectIds([id], locked);
+
+  const headDragMove = (e) => {
+    const x = e.target.x();
+    const dx = x - headX;
+    e.target.setAttrs({ y: headY });
+    const newWidth = isBlue ? headX + dx : width - x;
+    const newX = isBlue ? -strokeWidth / 2 : -strokeWidth / 2 + dx;
+    const newIsBlue = isBlue ? dx > -width : dx > width;
+    const newColor = newIsBlue ? colors.blue : colors.red;
+    arc.current.setAttrs({
+      x: newX,
+      data: arcData(newWidth, height),
+      fill: newColor,
+    });
+    head.current.setAttrs({ fill: newColor, stroke: newColor });
+    rect.current.setAttrs({ width: newWidth, x: newX + strokeWidth / 2 });
+  };
+
+  const headDragEnd = (e) => {
+    const x = e.target.x();
+    const dx = x - headX;
+    e.target.setAttrs({ x: 0, y: headY });
+    const newWidth = isBlue ? headX + dx : width - x;
+    const newX = isBlue ? pos.x - origin.x : pos.x + dx - origin.x;
+    arc.current.setAttrs({
+      x: -strokeWidth / 2,
+    });
+    rect.current.setAttrs({ x: 0, y: 0 });
+    updateElement(id, { x: newX, width: newWidth });
+  };
+
+  const resizeHandleDragMove = (e) => {
+    const dy = Math.min(e.target.y() + rhHeight / 2, height - minHeight);
+    e.target.setAttrs({ x: width / 2 - rhWidth / 2, y: -rhHeight / 2 + dy });
+    const newHeight = height - dy;
+    arc.current.setAttrs({ data: arcData(width, newHeight) });
+  };
+  const resizeHandleDragEnd = (e) => {
+    const dy = Math.min(e.target.y() + rhHeight / 2, height - minHeight);
+    e.target.setAttrs({ x: width / 2 - rhWidth / 2, y: -rhHeight / 2 });
+    const newHeight = height - dy;
+    updateElement(id, { y: pos.y - origin.y + dy, height: newHeight });
+    arc.current.setAttrs({ data: arcData(width, newHeight) });
+  };
+
   return (
     <Group
       ref={group}
@@ -50,16 +111,9 @@ const Arrow = ({ id, x, y, width, height, isBlue, visible, locked }) => {
       y={pos.y}
       visible={visible !== undefined ? visible : true}
       draggable
-      onDragMove={(e) => {
-        if (e.target === group.current) {
-        }
-      }}
-      onDragEnd={(e) => {
-        if (e.target === group.current) {
-          relocateElement(id, e.target.x() - pos.x, e.target.y() - pos.y);
-        }
-      }}
-      onPointerClick={() => selectIds([id], locked)}
+      onDragMove={groupDragMove}
+      onDragEnd={groupDragEnd}
+      onPointerClick={groupClick}
     >
       <Rect ref={rect} width={width} height={height} stroke={"black"} />
       <Path ref={arc} x={-strokeWidth / 2} y={height} fill={color} data={arcData(width, height)} />
@@ -76,59 +130,18 @@ const Arrow = ({ id, x, y, width, height, isBlue, visible, locked }) => {
           l ${-headSize / 2} ${headSize}
         `}
         draggable
-        onDragMove={(e) => {
-          const x = e.target.x();
-          const dx = x - headX;
-          e.target.setAttrs({ y: headY });
-          const newWidth = isBlue ? headX + dx : width - x;
-          const newX = isBlue ? -strokeWidth / 2 : -strokeWidth / 2 + dx;
-          const newIsBlue = isBlue ? dx > -width : dx > width;
-          const newColor = newIsBlue ? colors.blue : colors.red;
-          arc.current.setAttrs({
-            x: newX,
-            data: arcData(newWidth, height),
-            fill: newColor,
-          });
-          head.current.setAttrs({ fill: newColor, stroke: newColor });
-          rect.current.setAttrs({ width: newWidth, x: newX + strokeWidth / 2 });
-        }}
-        onDragEnd={(e) => {
-          const x = e.target.x();
-          const dx = x - headX;
-          e.target.setAttrs({ x: 0, y: headY });
-          const newWidth = isBlue ? headX + dx : width - x;
-          const newX = isBlue ? pos.x - origin.x : pos.x + dx - origin.x;
-          arc.current.setAttrs({
-            x: -strokeWidth / 2,
-          });
-          rect.current.setAttrs({ x: 0, y: 0 });
-          updateElement(id, { x: newX, width: newWidth });
-        }}
+        onDragMove={headDragMove}
+        onDragEnd={headDragEnd}
       />
-      <Circle
-        x={width / 2}
-        y={0}
-        radius={6}
+      <Rect
+        x={width / 2 - rhWidth / 2}
+        y={-rhHeight / 2}
+        width={rhWidth}
+        height={rhHeight}
+        stroke={"black"}
         draggable
-        onPointerEnter={(e) => e.target.setAttrs({ stroke: colors.blue, fill: colors.white })}
-        onPointerLeave={(e) => e.target.setAttrs({ stroke: null, fill: null })}
-        onDragMove={(e) => {
-          const dy = Math.min(e.target.y(), height - minHeight);
-          e.target.setAttrs({ x: width / 2, y: dy });
-          const newHeight = height - dy;
-          arc.current.setAttrs({
-            data: arcData(width, newHeight),
-          });
-        }}
-        onDragEnd={(e) => {
-          const dy = Math.min(e.target.y(), height - minHeight);
-          e.target.setAttrs({ x: width / 2, y: 0 });
-          const newHeight = height - dy;
-          updateElement(id, { y: pos.y - origin.y + dy, height: newHeight });
-          arc.current.setAttrs({
-            data: arcData(width, newHeight),
-          });
-        }}
+        onDragMove={resizeHandleDragMove}
+        onDragEnd={resizeHandleDragEnd}
       />
     </Group>
   );
