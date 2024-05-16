8b751b70d58c5bdeaa93d9c9ea1e7bb3980d372b
Adil Gabdullin <izenbard@gmail.com>
2024-05-17 01:47:05 +0500

    tracker

--- src/components/Wall.jsx
+++ src/components/Wall.jsx
@@ -1,7 +1,7 @@
-import { Group, Line, Rect } from "react-konva";
+import { Circle, Group, Line } from "react-konva";
 import { colors, config, workspace } from "../config";
 import { useAppStore } from "../state/store";
-import { halfPixel, pointsIsClose } from "../util";
+import { halfPixel, numberBetween, pointsIsClose } from "../util";
 import { NotchText } from "./Notches";
 
 const size = config.tile.size;
@@ -18,7 +18,7 @@ const lineProps2 = {
 
 const Wall = ({}) => {
   const state = useAppStore();
-  const { origin, showWallLine } = state;
+  const { origin } = state;
   const rect = wallRect(state);
   const { x, y, width, height } = rect;
   const ys = [];
@@ -34,7 +34,8 @@ const Wall = ({}) => {
           <Line key={y} x={0} y={y} points={[0, 0, width, 0]} {...lineProps} />
         ))}
       </Group>
-      {showWallLine && <WallLine {...rect} />}
+      {state.showWallLine && <WallLine {...rect} />}
+      {state.showWallTracker && <Tracker {...rect} />}
     </>
   );
 };
@@ -54,7 +55,7 @@ const WallLine = ({ x, y, width, height }) => {
     <Group x={x} y={y + height + size}>
       <Line points={[0, 0, width, 0]} {...lineProps2} />
       <Line x={0} points={[0, 0, width, 0]} {...lineProps2} />
-      <Group x={0}>
+      <Group x={-1}>
         <Line points={[0, -bigNotch, 0, bigNotch]} {...lineProps2} />
         <NotchText height={10} denominator={1} text={0} />
       </Group>
@@ -68,6 +69,56 @@ const WallLine = ({ x, y, width, height }) => {
         <Line points={[0, -bigNotch, 0, bigNotch]} {...lineProps2} />
         <NotchText height={10} denominator={1} text={1} />
       </Group>
+    </Group>
+  );
+};
+
+const Tracker = ({ x, y, width, height }) => {
+  const pos = width - 1;
+  const { origin, fdMode, elements } = useAppStore();
+  x = Math.round(origin.x + x);
+  y = Math.round(origin.y + y);
+  const minX = x;
+  const maxX = x + width - 1;
+
+  const color = colors.blue;
+  const sens = 12;
+
+  const events = {
+    draggable: !fdMode,
+    onDragMove: (e) => {
+      let tx = e.target.x();
+      tx = Math.min(maxX, tx);
+      tx = Math.max(minX, tx);
+      const step = Math.round(width / 10);
+      for (let snap = x - 1; snap < x + width; snap += step) {
+        if (numberBetween(tx, snap - sens, snap + sens)) {
+          tx = snap;
+          break;
+        }
+      }
+      for (const tile of Object.values(elements)) {
+        if (tile.type != "tile") continue;
+        let snap = origin.x + tile.x;
+        if (numberBetween(tx, snap - sens, snap + sens)) {
+          tx = snap;
+          break;
+        }
+        snap = origin.x + tile.x + tile.width - 1;
+        if (numberBetween(tx, snap - sens, snap + sens)) {
+          tx = snap;
+          break;
+        }
+      }
+      e.target.setAttrs({ x: Math.round(tx), y });
+    },
+  };
+  return (
+    <Group x={x + pos} y={y} {...events}>
+      <Circle x={0} y={-size / 2} fill={color} radius={12} />
+      <Line points={[0, -size / 2, 0, height]} strokeWidth={2} stroke={colors.blue} />
+      <Line points={[0, height + size - bigNotch, 0, height + size + bigNotch]} strokeWidth={2} stroke={colors.white} />
+      <Circle x={0} y={height + size} fill={color} radius={8} strokeWidth={2} stroke={colors.white} />
     </Group>
   );
 };
