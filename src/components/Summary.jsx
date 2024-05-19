c1c856cddf26adff280c078f8b0fe294355f3f4b
Adil Gabdullin <izenbard@gmail.com>
2024-05-20 03:04:13 +0500

    summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,9 +1,8 @@
 import { Group, Rect, Text } from "react-konva";
-import { config, workspace } from "../config";
+import { colors, config, workspace } from "../config";
 import { useAppStore } from "../state/store";
 
 const margin = 16;
-const squareWidth = 20;
 const fontSize = 36;
 const height = config.summary.height;
 
@@ -12,56 +11,51 @@ const Summary = () => {
   if (!state.showSummary || !state.width || state.workspace == workspace.ppw) {
     return null;
   }
-  const sums = getSums(state.elements);
-  const colors = config.colors;
-  const widths = sums.map((s) => (s.count < 10 ? 40 : 60));
-  const width = (sums.length + 1) * margin + widths.reduce((sum, w) => sum + w, 0);
-  const xs = [margin];
-  for (let i = 1; i < sums.length; i += 1) {
-    xs.push(xs[i - 1] + widths[i - 1] + margin);
-  }
-  const y = (height - squareWidth) / 2;
+  const text = getSummary(state.elements);
+  const width = text.length * 14 + margin * 2;
   return (
-    <Group x={(state.width + config.leftToolbar.width - width) / 2} y={state.height - 86} visible={sums.length > 0}>
+    <Group x={(state.width + config.leftToolbar.width - width) / 2} y={state.height - 86} visible={text != ""}>
       <Rect width={width} height={height} strokeWidth={4} cornerRadius={8} stroke={colors.grey} fill={colors.white} />
-      {sums.map(({ color, count }, i) => (
-        <Item key={color} color={color} count={count} x={xs[i]} y={y} />
-      ))}
+      <Text
+        text={text}
+        y={height / 2 - fontSize / 2}
+        width={width}
+        fontFamily="Calibri"
+        fontSize={fontSize}
+        align="center"
+        fill={colors.blue}
+        stroke={colors.blue}
+      />
     </Group>
   );
 };
 
-const Item = ({ x, y, color, count }) => {
-  return (
-    <>
-      <Rect x={x} y={y} fill={color} width={squareWidth} height={squareWidth} />
-      <Text
-        x={x + 25}
-        y={(height - fontSize) / 2 + 2}
-        text={count.toString()}
-        fontFamily="Calibri"
-        fontSize={fontSize}
-        fill={color}
-        stroke={color}
-      />
-    </>
-  );
-};
-
-function getSums(elements) {
-  const sums = {};
-  for (const { fill } of config.tile.options) {
-    sums[fill] = { color: fill, count: 0 };
+function getSummary(elements) {
+  const counts = {};
+  for (const { text } of config.tile.options) {
+    counts[text] = 0;
   }
   for (const tile of Object.values(elements)) {
     if (tile.type == "tile") {
-      sums[tile.fillColor].count += 1;
+      counts[tile.text] += 1;
     }
   }
-  for (const key in sums) {
-    if (sums[key].count == 0) delete sums[key];
+  const plus = counts["+"];
+  const minus = counts["-"];
+
+  if (plus == 0 && minus == 0) {
+    return "";
   }
-  return Object.values(sums);
+  if (plus > 0 && minus == 0) {
+    return `${plus}`;
+  }
+  if (plus == 0 && minus > 0) {
+    return `(${-minus})`;
+  }
+  if (plus > 0 && minus > 0) {
+    return `${plus} + (${-minus})`;
+  }
+  console.error("unreachable");
 }
 
 export default Summary;
